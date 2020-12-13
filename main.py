141354279f3961cd07e8bec88389443c6a6a82c5
Prabal Rawal <74870456+Prabal-Rawal@users.noreply.github.com>
2020-12-13 11:08:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import random
 
 def game():
-    user = input("What's your choice?\n's'for scissor,'r'for rock and 'p'for paper\nYour choice: ")
-    pc = random.choice(['r','p','s'])
+    user = input("What's your choice?\n's'for scissor,'r'for rock and 'p'for paper\nYour choice: ")  #we take user input i.e rock,paper or scissor
+    pc = random.choice(['r','p','s'])  #The computer randomly chooses from one of the chice
     winner(user,pc)
 
 def winner(user,pc):
-    if (user=='r' and pc == 's') or (user=='p' and pc =='r') or (user=='s' and pc =='p'):
+    if (user=='r' and pc == 's') or (user=='p' and pc =='r') or (user=='s' and pc =='p'):  #we know how a player wins in the game 
         print("You won")
     
     elif (user == pc):
@@ -15,3 +15,6 @@ def winner(user,pc):
     else:
         print("you lost")
 
+        
+game()  #Calling thr function game
+       
